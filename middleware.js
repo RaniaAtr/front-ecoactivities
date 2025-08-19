4c87f7581036a4d5d775e4ad5784a93d54847f04
Rania <raniaattar1616@gmail.com>
2025-08-19 15:58:13 +0200

    correction pour creation activity

--- middleware.js
+++ middleware.js
@@ -1,18 +1,31 @@
+// app/middleware.js
 import { NextResponse } from 'next/server';
 import jwt from 'jsonwebtoken';
+
 export function middleware(request) {
+  // Récupère le token dans les cookies
   const token = request.cookies.get('token')?.value;
+
+  // Si pas de token -> redirige vers la page de login
   if (!token) {
     return NextResponse.redirect(new URL('/login', request.url));
   }
+
+  // Décode le token (JWT)
   const decoded = jwt.decode(token);
+  console.log(decoded)
+
   // Vérifie que l'utilisateur est admin
   if (!decoded || !decoded.roles?.includes('ROLE_ADMIN')) {
     return NextResponse.redirect(new URL('/unauthorized', request.url));
   }
+
+  // Si tout est ok, on continue
   return NextResponse.next();
 }
+
 // Appliquer le middleware uniquement aux routes admin
 export const config = {
-  matcher: ['/admin/:path*'],
+  matcher: ['/admin/:path*'], // protège toutes les routes sous /admin
 };
+
