dfa7fb023760e7dfb448483038be6dfde842695d
Rania <raniaattar1616@gmail.com>
2025-09-19 14:37:24 +0200

    modif import component

--- src/app/reset-password/page.js
+++ src/app/reset-password/page.js
@@ -1,5 +1,5 @@
 "use client";
-import ResetPasswordForm from "../components/ResetPasswordForm/";
+import ResetPasswordForm from "@/components/ResetPasswordForm";
 
 export default function ResetPasswordPage() {
   return (
