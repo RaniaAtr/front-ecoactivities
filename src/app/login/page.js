97456ea43a433054521d5362fdf70c7faac796dc
Rania <raniaattar1616@gmail.com>
2025-09-26 12:50:06 +0200

    m

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -18,7 +18,7 @@ export default function LoginPage() {
     setLoading(true);
 
     try {
-      const res = await fetch("http://localhost:3000/api/login", {
+      const res = await fetch("https://ecoactivities.fr/api/login", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(form),
