78c2734be2c8adfa87ff2acd75551f3cc16c9f0c
Rania <raniaattar1616@gmail.com>
2025-09-26 12:37:06 +0200

    m

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -26,7 +26,7 @@ export default function RegisterPage() {
     setLoading(true);
 
     try {
-      const res = await fetch("http://localhost:3000/api/register", {
+      const res = await fetch("https://ecoactivities.fr/api/register", {
         method: "POST",
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify(form)
